deb2223e3ebb3b1498989025473b598722e95153
주재민 <jujaemin@jujaemin-ui-MacBookAir.local>
2023-04-17 11:18:41 +0900

    ㅁㅁ

--- frontend/src/pages/Questions.jsx
+++ frontend/src/pages/Questions.jsx
@@ -1,50 +1,20 @@
 import styled from "styled-components";
 import AllQuestions from "../components/AllQuestions";
-import YellowBlock from "../components/YellowBlock";
-import Collectives from "../components/Collectives";
-import RelatedTags from "../components/RelatedTags";
-import HotQuestions from "../components/HotQuestions";
+import Nav from "../components/Nav";
 
 const Wrapper = styled.section`
-	width: 100vw;
-	height: auto;
-	display: flex;
-	padding-bottom: 50px;
-	position: relative;
-`;
-
-const Container = styled.section`
-	height: auto;
-	margin-top: 50px;
-	display: flex;
-	justify-content: left;
-	align-items: start;
-	margin-left: 60px;
-	z-index: 5;
-`;
-
-const Aside = styled.aside`
-	width: auto;
-	height: auto;
-	display: grid;
-	grid-template-rows: repeat(auto, 1fr);
-	gap: 20px;
-	margin-left: 25px;
-	margin-top: 50px;
+  height: auto;
+  display: flex;
+  align-items: start;
+  padding-bottom: 50px;
+  position: relative;
 `;
 
 export default function Questions() {
-	return (
-		<Wrapper>
-			<Container>
-				<AllQuestions />
-				<Aside>
-					<YellowBlock />
-					<Collectives />
-					<RelatedTags />
-					<HotQuestions />
-				</Aside>
-			</Container>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Nav />
+      <AllQuestions />
+    </Wrapper>
+  );
 }
