cd424f95c2e74dd9e36065d8a7735c7bfe4a2117
Latemarch <dolphin0331@naver.com>
2023-04-18 21:44:55 +0700

    Update Tags.jsx

--- frontend/src/pages/Tags.jsx
+++ frontend/src/pages/Tags.jsx
@@ -7,25 +7,25 @@ import { StackExchange } from "../utils/stackExchangeApi";
 import Nav from "../components/Nav";
 
 const TagsWrapper = styled.div`
-  padding: 20px;
+	padding: 20px;
 `;
 const TextContainer = styled.div`
-  margin-bottom: 30px;
+	margin-bottom: 30px;
 `;
 const H1 = styled.h1`
-  margin-top: 10px;
-  margin-bottom: 20px;
-  font-size: 30px;
+	margin-top: 10px;
+	margin-bottom: 20px;
+	font-size: 30px;
 `;
 const Span = styled.span`
-  font-size: ${({ theme }) => theme.fontSizes.lg};
-  padding-bottom: 20px;
+	font-size: ${({ theme }) => theme.fontSizes.lg};
+	padding-bottom: 20px;
 `;
 const IconContainer = styled.div`
-  position: absolute;
-  top: 8px;
-  left: 8px;
-  color: gray;
+	position: absolute;
+	top: 8px;
+	left: 8px;
+	color: gray;
 `;
 
 export default function Tags() {
