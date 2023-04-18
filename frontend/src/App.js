2639c01e2f35706746eb73582fbfd9d346362e61
Latemarch <dolphin0331@naver.com>
2023-04-18 21:47:51 +0700

    세부사항 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 
 const Body = styled.div`
