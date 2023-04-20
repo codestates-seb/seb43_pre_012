c44d45bfc44d6e7b1a834a0c18a25bcacb3edabd
nowaveosu <nowaveosu@gmail.com>
2023-04-20 16:30:27 +0900

    헤더버튼3개  Link 추가

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'; 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -7,21 +8,21 @@ import { Link } from "react-router-dom";
 import { Fade as Hamburger } from 'hamburger-react'
 
 const HeaderWrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-background-color: white;
-color: #fffefe;
-height: 50px;
-border-top : 3px solid ${({ theme }) => theme.colors.orange};
-box-shadow : 0 0 3px gray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    color: #fffefe;
+    height: 50px;
+    border-top : 3px solid ${({ theme }) => theme.colors.orange};
+    box-shadow : 0 0 3px gray;
 
 `;
 const Logo = styled.img`
-width: 200px;
-padding-right : 40px;
-&.smallLogo{
-    display : none;
+    width: 200px;
+    padding-right : 40px;
+    &.smallLogo{
+        display : none;
 }
 @media screen and (max-width: ${({ theme }) => theme.screen.md}) {
     &.largeLogo {
@@ -41,25 +42,25 @@ padding-right : 40px;
 }
 `;
 const Menu = styled.a`
-font-size: 15px;
-padding-right: 22px;
-color : black;
-white-space: nowrap;
-text-decoration-line: none;
-@media screen and (max-width: ${({ theme }) => theme.screen.md}) {
-    &.about, &.forteams {
-        display : none;
-    }
+    font-size: 15px;
+    padding-right: 22px;
+    color : black;
+    white-space: nowrap;
+    text-decoration-line: none;
+    @media screen and (max-width: ${({ theme }) => theme.screen.md}) {
+        &.about, &.forteams {
+            display : none;
+        }
 }
 
 `;
 const Scope = styled.div`
-display : none;
-font-size : 20px;
-padding-right : 9px;
-@media screen and (max-width: ${({ theme }) => theme.screen.sm}) {
-    display : block;
-}
+    display : none;
+    font-size : 20px;
+    padding-right : 9px;
+    @media screen and (max-width: ${({ theme }) => theme.screen.sm}) {
+        display : block;
+    }
 
 `
 
@@ -102,43 +103,57 @@ const SearchBar = styled.div`
         display : none;
     }
 `
-const Button = styled.button`
-height : 34px;
-width : 60px;
-display: flex;
-justify-content: center;
-align-items: center;
-cursor: pointer;
-border : 1px solid #7AA8C7;
-margin-right : 5px;
-border-radius : 3px;
-white-space: nowrap;
-&.login{
-    background-color : ${({ theme }) => theme.colors.tagblue};
-    color : #7AA8C7;
-}
-&.signup{
-    background-color : ${({ theme }) => theme.colors.skyblue};
-    color : white;
-}
 
-`
-;
+const Button = styled.button`
+    height : 34px;
+    width : 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border : 1px solid #7AA8C7;
+    margin-right : 5px;
+    border-radius : 3px;
+    white-space: nowrap;
+    &.login{
+        background-color : ${({ theme }) => theme.colors.tagblue};
+        color : #7AA8C7;
+    }
+    &.signup{
+        background-color : ${({ theme }) => theme.colors.skyblue};
+        color : white;
+    }
+
+`;
+
+
+
 
 
 export default function Header() {
 
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggleModal = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <HeaderWrapper>
-            <Hamburger color='black' size={18} />
+            <Hamburger color='black' size={18} onClick={toggleModal} />
             <Link to = "/">
                 <Logo className='largeLogo' src={largeLogo} />
                 <Logo className='smallLogo' src={smallLogo} />
             </Link>
-            <Menu className='about' href="About.js">About</Menu>
-            <Menu className='products' href="Products.js">Products</Menu>
-            <Menu className='forteams' href="ForTeams">For Teams</Menu>
-            
+            <Link to="Questions">
+                <Menu >Questions</Menu>
+			</Link>
+            <Link to="Tags">
+                <Menu >Tags</Menu>
+			</Link>
+            <Link to="Companies">
+                <Menu >Companies</Menu>
+			</Link>
             <SearchBar>
                 <FontAwesomeIcon className = "searchIcon" icon={faMagnifyingGlass} color='#838C95'/>
                 <input className="search-bar__input" type="search" placeholder="Search..." />
@@ -152,6 +167,7 @@ export default function Header() {
             <Link to="signup">
                 <Button className='signup'>Sign up</Button>
 			</Link>
+
         </HeaderWrapper>
     )
 }
