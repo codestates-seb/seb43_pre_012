a2c25b2b490b7fe0bb30af5ad00f9922833254ca
mikiehw <108291609+mikiehw@users.noreply.github.com>
2023-04-20 16:04:09 +0900

    JWT 로그인 인증 로직 구현 (#96)
    * SpringSecurity & JWT (#91)
    
    * Back/feat/spring security hw  application.yml 수정 (#92)
    
    * SpringSecurity & JWT
    
    * application.yml - jwt information

---
+++ backend/preproject/src/main/java/com/seb43_pre_12/preproject/auth/MemberDetailsService.java
@@ -0,0 +1,73 @@
+package com.seb43_pre_12.preproject.auth;
+
+import com.seb43_pre_12.preproject.exception.BusinessLogicException;
+import com.seb43_pre_12.preproject.exception.ExceptionCode;
+import com.seb43_pre_12.preproject.member.entity.Member;
+import com.seb43_pre_12.preproject.member.repositoy.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+// SpringSecurity 로그인 인증시에 AuthenticationManager에게 member의 UserDetail 정보 조회 task 를 위임 받아 수행하는 클래스
+@Component
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public MemberDetailsService(MemberRepository memberRepository, CustomAuthorityUtils authorityUtils) {
+        this.memberRepository = memberRepository;
+        this.authorityUtils = authorityUtils;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> findMember = memberRepository.findByEmail(username);
+        Member member = findMember.orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new MemberDetatils(member);
+    }
+    private final class MemberDetatils extends Member implements UserDetails {
+        public MemberDetatils(Member m) {
+            setMemberId(m.getMemberId());
+            setEmail(m.getEmail());
+            setUsername(m.getUsername());
+            setPassword(m.getPassword());
+            setRoles(m.getRoles());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getUsername() {
+            return getUsername();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return false;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return false;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return false;
+        }
+    }
+}
---
+++ backend/preproject/src/main/java/com/seb43_pre_12/preproject/config/SecurityConfiguration.java
@@ -0,0 +1,79 @@
+package com.seb43_pre_12.preproject.config;
+
+import com.seb43_pre_12.preproject.auth.JwtAuthenticationFilter;
+import com.seb43_pre_12.preproject.auth.JwtTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer; // 커스텀 필터 설정 클래스(configurer)를 만들 때 JwtAuthenticationFilter 객체 생성을 위해 필요함.
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()  // 스프링시큐리티는 csrf 공격방지를 위해, csrf token 을 수신후 검증함.
+                .cors(Customizer.withDefaults())
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomFilterConfigurer()) // apply() 메서드를 이용해서 커스터마이징 Configuration을 추가.
+                .and()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+        return httpSecurity.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));
+
+        //UrlBasedCorsConfigurationSource 는 CorsConfigurationSource 인터페이스를 구현한 클래스이다.
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // 커스텀 필터 설정기(Configurer)를 만들기 위한 코드. - 설정기의 객체를 apply메서드에 넣어주면 SpringSecurityFilter에 원하는 필터를 추가할수있다.
+    // SpringSecurityFilter에 필터를 추가하기 위해서는 설정기(Configurur)를 이용해야한다 라고 개념적으로 인식하면됨.
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            // AuthenticationManager는 인터페이스인데, getSharedObject메서드를 통해서 AuthenticationManager의 객체를 얻을수 있는 듯.
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            // JwtAuthenticationFilter 객체 생성.
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager,jwtTokenizer);
+
+            // 쉽게 말하면, 로그인 인증을 위한 엔드포인트(주소)를 설정하는 메서드임. setFilterProcessesUrl()
+            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+            // addFilter로 JwtAuthenticationFilter 를 spring security filter에 추가할 수 있음.
+            // 궁극적으로는 설정기의 객체를 apply() 메서드에 넣어줘야함.
+            builder.addFilter(jwtAuthenticationFilter);
+        }
+    }
+}
